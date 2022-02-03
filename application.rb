323c7c6d5fefc7d02356c685eee36136a3f7fc1b
GabrielMalakias <gabriel07malakias@gmail.com>
2022-02-03 10:13:45 +0100

    Testing the * match

--- application.rb
+++ application.rb
@@ -14,9 +14,13 @@ loader.push_dir('./lib')
 loader.setup
 loader.eager_load
 
+test = proc do |request|
+  [200, {'Content-Type' => 'application/text'}, ["String"]]
+end
 
 # Server and routing
 Agoo::Server.init(3000, 'root')
 Agoo::Server.handle(:POST, "/authors", Handler::CreateAuthor)
+Agoo::Server.handle(:GET, "/authors/*/new", test)
 Agoo::Server.handle(:GET, "/authors/*", Handler::FindAuthor)
 Agoo::Server.start()
